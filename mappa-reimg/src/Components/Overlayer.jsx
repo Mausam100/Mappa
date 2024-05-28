f5f734cddaa2f1ee89b75f1f0aa08fe5cb9039ed
omkar-4 <omkar.pro04@gmail.com>
2024-05-28 23:39:50 +0530

    working on cards:size issues

--- mappa-reimg/src/Components/Overlayer.jsx
+++ mappa-reimg/src/Components/Overlayer.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 
+
 const Overlayer = ({ card, toggleOverlay }) => {
   if (!card) return null;
 
   return (
-    <div className="overlay fixed z-40 left-0 top-0 w-full h-full bg-[#000000dc] flex" onClick={toggleOverlay}>
-      <div className='w-1/2 h-full justify-center items-center mt-16'>
-        <h1 className="text-white text-4xl">{card.title}</h1>
-        <p className="text-white mt-4">{card.description}</p>
-        <a href={card.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
-            Watch Now
-          </a>
-      </div>
-      <div className='w-1/2 h-full flex justify-center items-center'>
-        <div>
-          <img src={card.image} alt={card.title} className=" h-[70vh] mt-10 w-[20vw]" />
-          
-          
+    <>
+    <div className="overlay fixed z-40 left-0 top-0 w-[100%] h-[100%] bg-[#000000] flex" onClick={toggleOverlay}>
+      <div className='flex items-center justify-between  w-full h-full'>
+        <div className='w-[70%] p-20 h-full bg-pink-300'>hello</div>
+        <div className="w-[30%] h-full bg-sky-300 relative">
+          <img src={card.image} alt={card.title} className='w-[600px] h-[578px] absolute'/>hello
         </div>
       </div>
     </div>
+    </>
   );
 };
 
